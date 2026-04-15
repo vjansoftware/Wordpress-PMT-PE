21e6b57be92871f34007cab01eeeea565a87a811
vjceo <154865662+vjceo@users.noreply.github.com>
2026-04-15 13:52:20 +0530

    Add enqueue function for parent theme styles

--- wp-content/themes/university-hub-child/functions.php
+++ wp-content/themes/university-hub-child/functions.php
@@ -1,1 +1,5 @@
-
+<?php
+function university_hub_child_enqueue() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'university_hub_child_enqueue');
